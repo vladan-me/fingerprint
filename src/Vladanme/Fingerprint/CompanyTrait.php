f62facda8e8fbc4f0bfc8eb0549564b070741969
Vladan Djokic <vladan.me@gmail.com>
2017-11-24 20:10:29 +0200

    Refactor, add traits so it can be reusable in fingerprint-elasticsearch project

---
+++ src/Vladanme/Fingerprint/CompanyTrait.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Vladanme\Fingerprint;
+
+trait CompanyTrait {
+// English stopwords that make sense for company names.
+  protected $eng_rem = ['and', 'at', 'by', 'for', 'in', 'of', 'on', 'the'];
+  protected $add_rem = ['pc', 'llc', 'llp', 'lp', 'plc', 'pllc'];
+  protected $add_syn = [
+    'acad'   => 'academy',
+    'assn'   => 'association',
+    'inst'   => 'institute',
+    'dept'   => 'department',
+    'fed'    => 'federal',
+    'assoc'  => 'associates',
+    'natl'   => 'national',
+    'univ'   => 'university',
+    'schl'   => 'school',
+    'schls'  => 'schools',
+    'supt'   => 'superintedent',
+    'dist'   => 'district',
+    'ctr'    => 'center',
+    'centre' => 'center',
+    'mgmt'   => 'management',
+    'svc'    => 'service',
+    'med'    => 'medical',
+    'tech'   => 'technology',
+    'co op'  => 'cooperative',
+    'mid'    => 'middle',
+    'mfg'    => 'manufacturing',
+    'ofc'    => 'office',
+  ];
+  protected $add_syn_rem = [
+    'corp' => 'corporation',
+    'co'   => 'company',
+    'cu'   => 'credit union',
+    'intl' => 'international',
+    'inc'  => 'incorporated',
+    'ltd'  => 'limited',
+    'na'   => 'north america',
+    'us'   => 'united states',
+    'usa'  => 'united states',
+  ];
+}
